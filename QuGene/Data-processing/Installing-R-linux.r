c3261b8fcd2145506a04d5051796830f0a166aee
Jenniferlin3 <54757097+Jenniferlin3@users.noreply.github.com>
2020-07-06 20:59:04 -0400

    Create Installing-R-linux.r

---
+++ QuGene/Data-processing/Installing-R-linux.r
@@ -0,0 +1,11 @@
+### Install the packages into library on compute canada
+
+install.packages("readr", dependencies=TRUE, INSTALL_opts='--no-lock')
+
+install.packages("dplyr", dependencies=TRUE, INSTALL_opts='--no-lock')
+
+install.packages("stringr", dependencies=TRUE, INSTALL_opts='--no-lock')
+
+install.packages("openxlsx", dependencies=TRUE, INSTALL_opts='--no-lock')
+
+install.packages("tidyverse", dependencies=TRUE, INSTALL_opts='--no-lock')
