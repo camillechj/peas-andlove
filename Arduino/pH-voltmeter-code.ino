3f99ef85b5efb36d010bbd0a17d64caba764c812
Jenniferlin3 <54757097+Jenniferlin3@users.noreply.github.com>
2021-06-01 22:22:48 -0400

    Update pH-voltmeter-code.ino

--- Arduino/pH-voltmeter-code.ino
+++ Arduino/pH-voltmeter-code.ino
@@ -26,17 +26,14 @@ void setup(){
 
 void loop(){
   value = analogRead(A0);
-  //voltage = value * 1.1/1023 * 1000;
   voltage = value * 5.0/1023 * 1000;
-  //voltage = value;
-  //voltage = value * 5.0/1023;
-  Serial.print("Time: ");
   time = (millis()/1000);
+  Serial.print("Time: ");
   Serial.println(time);
-  Serial.print("Voltage= ");
+  Serial.print("Voltage: ");
   Serial.println(voltage);
 
-  myFile = SD.open("volt-10.txt", FILE_WRITE);
+  myFile = SD.open("example.txt", FILE_WRITE);
   if (myFile) {
     myFile.print(time);
     myFile.print(",");
@@ -46,7 +43,7 @@ void loop(){
 
   // if the file didn't open, print an error:
   else {
-    Serial.println("error opening voltage2.txt");
+    Serial.println("error opening example.txt");
   }
   
   delay(5000);
@@ -57,4 +54,4 @@ void loop(){
 // 1) The file name CANNOT exceed 8 characters (including special characters)
 // 2) You must name a new file after each upload (the files are NEVER overwritten or appended to)
 // 3) If the Arduino accidentally powers off, you will need to re-upload the code. Use a new file name, or the new data will be lost
-// 4) An EBL rechargeable battery will only last 2 hours and 50 minutes. Need to determine a long term power solution.
+// 4) An EBL rechargeable battery will only last 2 hours and 50 minutes. Need to determine a long term cordless power solution
